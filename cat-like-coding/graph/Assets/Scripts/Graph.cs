d297477a4b2767d9dca6da4a28c7b56a78635281
emepi <emkemp@utu.fi>
2021-02-08 23:55:58 +0200

    part 3 mathematical surfaces

--- cat-like-coding/graph/Assets/Scripts/Graph.cs
+++ cat-like-coding/graph/Assets/Scripts/Graph.cs
@@ -1,25 +1,26 @@
 ﻿using UnityEngine;
 
 public class Graph : MonoBehaviour
-{
+{	
     [SerializeField]
 	Transform pointPrefab = default;
 	
 	[SerializeField, Range(10, 100)]
 	int resolution = 10;
 	
+	[SerializeField]
+	FunctionLibrary.FunctionName function = default;
+	
 	Transform[] points;
 	
 	void Awake () {
 		float step = 2f / resolution;
-		var position = Vector3.zero;
 		var scale = Vector3.one * step;
-		points = new Transform[resolution];
+		
+		points = new Transform[resolution * resolution];
 		
 		for (int i = 0; i < points.Length; i++) {
 			Transform point = Instantiate(pointPrefab);
-			position.x = (i + 0.5f) * step - 1f;
-			point.localPosition = position;
 			point.localScale = scale;
 			point.SetParent(transform, false);
 			points[i] = point;
@@ -27,13 +28,18 @@ public class Graph : MonoBehaviour
 	}
 	
 	void Update() {
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
 		float time = Time.time;
+		float step = 2f / resolution;
+		float v = 0.5f * step - 1f;
 		
-		for (int i = 0; i < points.Length; i++) {
-			Transform point = points[i];
-			Vector3 position = point.localPosition;
-			position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-			point.localPosition = position;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+			if (x == resolution) {
+				x = 0;
+				z += 1;
+			}
+			float u = (x + 0.5f) * step - 1f;
+			points[i].localPosition = f(u, v, time);
 		}
 	}
 }
