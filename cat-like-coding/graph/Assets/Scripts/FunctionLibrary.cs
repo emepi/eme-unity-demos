0cfbac271f331d98540e809209d6d62196c975de
emepi <emkemp@utu.fi>
2021-02-10 20:21:48 +0200

    v-dim bug fix

--- cat-like-coding/graph/Assets/Scripts/FunctionLibrary.cs
+++ cat-like-coding/graph/Assets/Scripts/FunctionLibrary.cs
@@ -13,18 +13,19 @@ public static class FunctionLibrary {
 	}
 	
 	public static Vector3 Torus (float u, float v, float t) {
-		float r = 1f;
-		float s = 0.5f + r * Cos(0.5f * PI * v);
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+		float s = r1 + r2 * Cos(PI * v);
 		Vector3 p;
 		p.x = s * Sin(PI * u);
-		p.y = r * Sin(0.5f * PI * v);
+		p.y = r2 * Sin(PI * v);
 		p.z = s * Cos(PI * u);
 		return p;
 	}
 	
 	public static Vector3 Sphere (float u, float v, float t) {
-		float r = 0.5f + 0.5f * Sin(PI * t);
-		float s = r * Cos(0.5f * PI *v);
+		float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+		float s = r * Cos(0.5f * PI * v);
 		Vector3 p;
 		p.x = s * Sin(PI * u);
 		p.y = r * Sin(0.5f * PI * v);
--- cat-like-coding/graph/Assets/Scripts/Graph.cs
+++ cat-like-coding/graph/Assets/Scripts/Graph.cs
@@ -16,7 +16,6 @@ public class Graph : MonoBehaviour
 	void Awake () {
 		float step = 2f / resolution;
 		var scale = Vector3.one * step;
-		
 		points = new Transform[resolution * resolution];
 		
 		for (int i = 0; i < points.Length; i++) {
@@ -31,7 +30,6 @@ public class Graph : MonoBehaviour
 		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
 		float time = Time.time;
 		float step = 2f / resolution;
-		float v = 0.5f * step - 1f;
 		
 		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
 			if (x == resolution) {
@@ -39,6 +37,7 @@ public class Graph : MonoBehaviour
 				z += 1;
 			}
 			float u = (x + 0.5f) * step - 1f;
+			float v = (z + 0.5f) * step - 1f;
 			points[i].localPosition = f(u, v, time);
 		}
 	}
